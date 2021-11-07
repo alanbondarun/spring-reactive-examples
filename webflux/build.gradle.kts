143d5bdb03f5b6c3736d7d071080a20d3b95e177
alanb <alanbondarun@scatterlab.co.kr>
2021-11-07 17:10:36 +0900

    feat: simulate long running tasks in webflux

--- webflux/build.gradle.kts
+++ webflux/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 
     implementation("org.springframework.boot:spring-boot-starter-webflux:2.5.6")
 }
--- webflux/src/main/kotlin/com/example/Application.kt
+++ webflux/src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,53 @@
 package com.example
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import kotlin.random.Random
 
 @SpringBootApplication
 open class Application
+
+@RestController
+open class Controller(
+    private val longTaskService: LongTaskService,
+) {
+    data class TestResponse(
+        val data: String = "Hello",
+    )
+
+    data class TaskResponse(
+        val data: Int?,
+    )
+
+    @GetMapping("/test")
+    open fun test() = TestResponse()
+
+    @GetMapping("/task")
+    open fun task() = longTaskService.getResult()
+        .map { TaskResponse(it) }
+        .defaultIfEmpty(TaskResponse(null))
+}
+
+@Service
+class LongTaskService {
+    fun getResult(): Mono<Int> {
+        runBlocking {
+            delay(10000)
+        }
+
+        return if (Random.nextBoolean()) {
+            Mono.just(Random.nextInt(10))
+        } else {
+            Mono.empty()
+        }
+    }
+}
 
 fun main() {
     runApplication<Application>()
